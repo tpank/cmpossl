9b5d369dacdf1178b28f2f0ec01bd20ccc47182f
David von Oheimb <David.von.Oheimb@siemens.com>
2017-10-13 11:54:10 +0200

    improved fix on reaction to missing [cmp] section and documentation

--- apps/cmp.c
+++ apps/cmp.c
@@ -2193,14 +2193,12 @@ int cmp_main(int argc, char **argv)
                 BIO_printf(bio_err, "error on line %ld of config file '%s'\n",
                            errorline, configfile);
         } else {
-            if (NCONF_get_section(conf, opt_section)) {
-                if (!read_config())
-                    goto err;
-            } else {
-                BIO_printf(bio_err,
-                           "Cannot find [%.40s] section in config file '%s'\n",
+            if (!NCONF_get_section(conf, opt_section)) {
+                BIO_printf(bio_c_out, "Warning: no [%.40s] section found in config file '%s'; will thus use just [default] and unnamed section if present\n",
                            opt_section, configfile);
             }
+            if (!read_config())
+                goto err;
         }
     }
 
