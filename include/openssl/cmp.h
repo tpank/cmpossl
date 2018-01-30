fab1de018e7c6552ab916bf94617fd3da16b6728
David von Oheimb <David.von.Oheimb@siemens.com>
2018-01-30 14:24:47 +0100

    added -policies and -policies_critical option; adding policy OID parse error to lib

--- include/openssl/cmp.h
+++ include/openssl/cmp.h
@@ -539,6 +539,7 @@ CMP_PKIFREETEXT *CMP_CTX_statusString_get(CMP_CTX *ctx);
 # define CMP_CTX_OPT_VALIDITYDAYS 11
 # define CMP_CTX_OPT_IGNORE_KEYUSAGE 12
 # define CMP_CTX_OPT_SUBJECTALTNAME_NODEFAULT 13
+# define CMP_CTX_OPT_POLICIES_CRITICAL 14
 int CMP_CTX_set_option(CMP_CTX *ctx, const int opt, const int val);
 # if 0
 int CMP_CTX_push_freeText(CMP_CTX *ctx, const char *text);
