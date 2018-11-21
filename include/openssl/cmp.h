14dbce643311437476c6f49b5f53b32d38dd06f4
David von Oheimb <David.von.Oheimb@siemens.com>
2018-11-21 09:54:46 +0100

    fix various issues that came up during CRMF code review

--- include/openssl/cmp.h
+++ include/openssl/cmp.h
@@ -565,7 +565,7 @@ ASN1_OCTET_STRING *OSSL_CMP_CTX_get0_recipNonce(OSSL_CMP_CTX *ctx);
 #  define OSSL_CMP_CTX_OPT_IGNORE_KEYUSAGE 12
 #  define OSSL_CMP_CTX_OPT_SUBJECTALTNAME_NODEFAULT 13
 #  define OSSL_CMP_CTX_OPT_POLICIES_CRITICAL 14
-int OSSL_CMP_CTX_set_option(OSSL_CMP_CTX *ctx, const int opt, const int val);
+int OSSL_CMP_CTX_set_option(OSSL_CMP_CTX *ctx, int opt, int val);
 #  if 0 /* held for future implementation */
 /* int OSSL_CMP_CTX_push_freeText(OSSL_CMP_CTX *ctx, const char *text); */
 #  endif /* 0 */
