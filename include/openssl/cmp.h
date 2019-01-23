ff2409815781295fa2f7558639f7e7c9e3161a39
David von Oheimb <David.von.Oheimb@siemens.com>
2019-01-23 21:30:47 +0100

    extend OSSL_CMP_expired() to OSSL_CMP_cmp_timeframe()

--- include/openssl/cmp.h
+++ include/openssl/cmp.h
@@ -411,7 +411,8 @@ int OSSL_CMP_X509_STORE_add1_certs(X509_STORE *store, STACK_OF(X509) *certs,
 STACK_OF(X509) *OSSL_CMP_X509_STORE_get1_certs(const X509_STORE *store);
 
 /* cmp_vfy.c */
-int OSSL_CMP_expired(const ASN1_TIME *endtime, const X509_VERIFY_PARAM *vpm);
+int OSSL_CMP_cmp_timeframe(const ASN1_TIME *start,
+                           const ASN1_TIME *end,  const X509_VERIFY_PARAM *vpm);
 int OSSL_CMP_validate_msg(OSSL_CMP_CTX *ctx, const OSSL_CMP_MSG *msg);
 int OSSL_CMP_validate_cert_path(const OSSL_CMP_CTX *ctx,
                                 const X509_STORE *trusted_store,
