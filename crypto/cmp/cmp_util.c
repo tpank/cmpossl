37b949dea03091e44688c0707a04613f158d1fa3
David von Oheimb <David.von.Oheimb@siemens.com>
2019-05-13 18:57:34 +0200

    improve doc/comments of log functions; improve OSSL_CMP_sk_X509_add1_cert.pod

--- crypto/cmp/cmp_util.c
+++ crypto/cmp/cmp_util.c
@@ -108,11 +108,9 @@ int OSSL_CMP_log_printf(OSSL_cmp_log_cb_t log_fn,
 }
 
 /*
- * Appends text to the extra data field of the last error message in the queue,
- * after adding the optional separator unless data has been empty so far.
- * Note that, in contrast, ERR_add_error_data() simply
- * overwrites the previous contents of the data field.
+ * auxiliary function for error reporting via the OpenSSL error queue
  */
+
 void OSSL_CMP_add_error_txt(const char *separator, const char *txt)
 {
     const char *file;
@@ -167,7 +165,10 @@ void OSSL_CMP_add_error_txt(const char *separator, const char *txt)
     } while (*txt != '\0');
 }
 
-/* print OpenSSL and CMP errors via the given callback fn or OSSL_CMP_puts */
+/*
+ * functions manipulating lists of certificates etc.
+ */
+
 void OSSL_CMP_print_errors_cb(OSSL_cmp_log_cb_t log_fn)
 {
     unsigned long err;
@@ -191,10 +192,6 @@ void OSSL_CMP_print_errors_cb(OSSL_cmp_log_cb_t log_fn)
     }
 }
 
-/*-
- * Append/prepend cert to given list, optionally only if not already contained
- * returns 1 on success, 0 on error
- */
 int OSSL_CMP_sk_X509_add1_cert(STACK_OF(X509) *sk, X509 *cert,
                                int not_duplicate, int prepend)
 {
@@ -214,13 +211,6 @@ int OSSL_CMP_sk_X509_add1_cert(STACK_OF(X509) *sk, X509 *cert,
     return X509_up_ref(cert);
 }
 
-/*-
- * Append list of certificates from 'certs' to given list,
- * optionally only if not self-signed and
- * optionally only if not already contained.
- * The certs parameter may be NULL.
- * returns 1 on success, 0 on error
- */
 int OSSL_CMP_sk_X509_add1_certs(STACK_OF(X509) *sk, const STACK_OF(X509) *certs,
                                 int no_self_signed, int no_duplicates)
 {
@@ -241,11 +231,6 @@ int OSSL_CMP_sk_X509_add1_certs(STACK_OF(X509) *sk, const STACK_OF(X509) *certs,
     return 1;
 }
 
-/*
- * Add all or self-signed certificates from the given stack to given store.
- * certs parameter may be NULL.
- * returns 1 on success, 0 on error
- */
 int OSSL_CMP_X509_STORE_add1_certs(X509_STORE *store, STACK_OF(X509) *certs,
                                    int only_self_signed)
 {
@@ -265,10 +250,6 @@ int OSSL_CMP_X509_STORE_add1_certs(X509_STORE *store, STACK_OF(X509) *certs,
     return 1;
 }
 
-/*
- * Retrieves a copy of all certificates in the given store.
- * returns NULL on error
- */
 STACK_OF(X509) *OSSL_CMP_X509_STORE_get1_certs(X509_STORE *store)
 {
     int i;
@@ -314,11 +295,6 @@ X509_EXTENSIONS *OSSL_CMP_X509_EXTENSIONS_dup(const X509_EXTENSIONS *extin)
     return exts;
 }
 
-/*
- * free any previous value of the variable referenced via tgt
- * and assign either a copy of the src ASN1_OCTET_STRING or NULL.
- * returns 1 on success, 0 on error.
- */
 int OSSL_CMP_ASN1_OCTET_STRING_set1(ASN1_OCTET_STRING **tgt,
                                     const ASN1_OCTET_STRING *src)
 {
@@ -343,11 +319,6 @@ int OSSL_CMP_ASN1_OCTET_STRING_set1(ASN1_OCTET_STRING **tgt,
     return 0;
 }
 
-/*
- * free any previous value of the variable referenced via tgt
- * and assign either a copy of the byte string (with given length) or NULL.
- * returns 1 on success, 0 on error.
- */
 int OSSL_CMP_ASN1_OCTET_STRING_set1_bytes(ASN1_OCTET_STRING **tgt,
                                           const unsigned char *bytes, size_t len)
 {
