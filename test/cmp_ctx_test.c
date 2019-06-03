a96b492b2f7684af0cc3a5e4ce0314a6d8117433
David von Oheimb <David.von.Oheimb@siemens.com>
2019-06-03 19:33:46 +0200

    clean up cmp_ctx_test

--- test/cmp_ctx_test.c
+++ test/cmp_ctx_test.c
@@ -15,57 +15,40 @@
 
 typedef struct test_fixture {
     const char *test_case_name;
-    X509_EXTENSIONS *exts;
+    OSSL_CMP_CTX *ctx;
 } OSSL_CMP_CTX_TEST_FIXTURE;
+
+static void tear_down(OSSL_CMP_CTX_TEST_FIXTURE *fixture)
+{
+    if (fixture != NULL)
+        OSSL_CMP_CTX_free(fixture->ctx);
+    OPENSSL_free(fixture);
+}
 
 static OSSL_CMP_CTX_TEST_FIXTURE *set_up(const char *const test_case_name)
 {
     OSSL_CMP_CTX_TEST_FIXTURE *fixture;
-    int setup_ok = 0;
-    /* Allocate memory owned by the fixture, exit on error */
+
     if (!TEST_ptr(fixture = OPENSSL_zalloc(sizeof(*fixture)))
-            || !TEST_ptr(fixture->exts = sk_X509_EXTENSION_new_null()))
-        goto err;
-    fixture->test_case_name = test_case_name;
-
-    setup_ok = 1;
- err:
-    if (!setup_ok) {
-        ERR_print_errors_fp(stderr);
-        exit(EXIT_FAILURE);
+            || !TEST_ptr(fixture->ctx = OSSL_CMP_CTX_new())) {
+        tear_down(fixture);
+        return NULL;
     }
+    fixture->test_case_name = test_case_name;
     return fixture;
-}
-
-static void tear_down(OSSL_CMP_CTX_TEST_FIXTURE *fixture)
-{
-    sk_X509_EXTENSION_pop(fixture->exts);
-    OPENSSL_free(fixture);
 }
 
 static int execute_CTX_reqExtensions_have_SAN_test(OSSL_CMP_CTX_TEST_FIXTURE *
                                                    fixture)
 {
-    int good = 0;
-    OSSL_CMP_CTX *ctx = NULL;
-    if (!TEST_ptr(ctx = OSSL_CMP_CTX_new()))
-        return good;
-    if (TEST_false(OSSL_CMP_CTX_reqExtensions_have_SAN(ctx)))
-        if (TEST_true(OSSL_CMP_CTX_set0_reqExtensions(ctx, fixture->exts))) {
-            fixture->exts = NULL;
-            good = TEST_true(OSSL_CMP_CTX_reqExtensions_have_SAN(ctx));
-        }
-    OSSL_CMP_CTX_free(ctx);
-    return good;
-}
-
-static int test_CTX_reqExtensions_have_SAN(void)
-{
-    SETUP_TEST_FIXTURE(OSSL_CMP_CTX_TEST_FIXTURE, set_up);
     const int len = 16;
     unsigned char str[16/* len */];
     ASN1_OCTET_STRING *data = NULL;
     X509_EXTENSION *ext = NULL;
+    X509_EXTENSIONS *exts = NULL;
+
+    if (!TEST_false(OSSL_CMP_CTX_reqExtensions_have_SAN(fixture->ctx)))
+        return 0;
 
     if (!TEST_int_eq(1, RAND_bytes(str, len))
             || !TEST_ptr(data = ASN1_OCTET_STRING_new())
@@ -74,12 +57,22 @@ static int test_CTX_reqExtensions_have_SAN(void)
                          X509_EXTENSION_create_by_NID(NULL,
                                                       NID_subject_alt_name, 0,
                                                       data))
-            || !TEST_true(sk_X509_EXTENSION_push(fixture->exts, ext))) {
+            || !TEST_ptr(exts = sk_X509_EXTENSION_new_null())
+            || !TEST_true(sk_X509_EXTENSION_push(exts, ext))
+            || !TEST_true(OSSL_CMP_CTX_set0_reqExtensions(fixture->ctx,
+                                                          exts))) {
+        ASN1_OCTET_STRING_free(data);
         X509_EXTENSION_free(ext);
-        tear_down(fixture);
-        fixture = NULL;
+        sk_X509_EXTENSION_free(exts);
+        return 0;
     }
     ASN1_OCTET_STRING_free(data);
+    return TEST_true(OSSL_CMP_CTX_reqExtensions_have_SAN(fixture->ctx));
+}
+
+static int test_CTX_reqExtensions_have_SAN(void)
+{
+    SETUP_TEST_FIXTURE(OSSL_CMP_CTX_TEST_FIXTURE, set_up);
     EXECUTE_TEST(execute_CTX_reqExtensions_have_SAN_test, tear_down);
     return result;
 }
