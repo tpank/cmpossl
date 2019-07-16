2f66a85759e3da4cf90405b9d41361d2a29e1c42
David von Oheimb <David.von.Oheimb@siemens.com>
2019-07-16 16:14:55 +0200

    improve bugfix: cert_response() must not check private key consistency for P10CR

--- crypto/cmp/cmp_ses.c
+++ crypto/cmp/cmp_ses.c
@@ -510,7 +510,7 @@ static X509 *get1_cert_status(OSSL_CMP_CTX *ctx, int bodytype,
  * Regardless of success, caller is responsible for freeing *resp (unless NULL).
  */
 static int cert_response(OSSL_CMP_CTX *ctx, int rid, OSSL_CMP_MSG **resp,
-                         int func, int not_received)
+                         int func, int req_type, int not_received)
 {
     const EVP_PKEY *rkey = ctx->newPkey != NULL ? ctx->newPkey : ctx->pkey;
     int fail_info = 0; /* no failure */
@@ -576,7 +576,7 @@ static int cert_response(OSSL_CMP_CTX *ctx, int rid, OSSL_CMP_MSG **resp,
             && !CMP_CTX_set1_extraCertsIn(ctx, (*resp)->extraCerts))
         return 0;
 
-    if (func != CMP_F_OSSL_CMP_EXEC_P10CR_SES
+    if (req_type != OSSL_CMP_PKIBODY_P10CR
             && !(X509_check_private_key(ctx->newClCert, rkey))) {
         fail_info = 1 << OSSL_CMP_PKIFAILUREINFO_incorrectData;
         txt = "public key in new certificate does not match our private key";
@@ -662,7 +662,7 @@ static X509 *do_certreq_seq(OSSL_CMP_CTX *ctx, const char *type_string, int fn,
     if (!send_receive_check(ctx, req, type_string, fn, &rep, rep_type, rep_err))
         goto err;
 
-    if (!cert_response(ctx, rid, &rep, fn, rep_err))
+    if (!cert_response(ctx, rid, &rep, fn, req_type, rep_err))
         goto err;
 
     result = ctx->newClCert;
