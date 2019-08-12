23d0a1e5607462c49f677c92f77c1c30a08c3a95
Dr. David von Oheimb <David.von.Oheimb@siemens.com>
2019-08-12 09:21:26 +0200

    revert inadvertent change by Akretsch in crypto/x509/x509_set.c

--- crypto/x509/x509_set.c
+++ crypto/x509/x509_set.c
@@ -7,7 +7,6 @@
  * https://www.openssl.org/source/license.html
  */
 
-
 #include <stdio.h>
 #include "internal/cryptlib.h"
 #include "internal/refcount.h"
@@ -19,7 +18,6 @@
 #include "internal/asn1_int.h"
 #include "internal/x509_int.h"
 #include "x509_lcl.h"
-
 
 int X509_set_version(X509 *x, long version)
 {
