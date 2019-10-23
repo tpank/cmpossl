cb7e1d9803e9acdcc54c9d8d79ce8734d8f63517
Dr. David von Oheimb <David.von.Oheimb@siemens.com>
2019-10-23 09:29:35 +0200

    simplify control structure w.r.t. in_multiline_comment in util/openssl-format-check.pl

--- util/openssl-format-check.pl
+++ util/openssl-format-check.pl
@@ -47,9 +47,9 @@ while(<>) {
 #       $count = 0 if ($2 eq "/" && $3 eq "*"); # do not ignore indent on line starting comment: '/*'
         $count -= INDENT_LEVEL if ($2 eq "&" && $3 eq "&"); # line starting with &&
         $count -= INDENT_LEVEL if ($2 eq "|" && $3 eq "|"); # line starting with ||
-    }
-    if (!$in_multiline_comment && $hanging_indent == -1) {
-        $count-- if (m/^(\s*)([a-z_0-9]+):/ && $2 ne "default"); # label
+        if ($hanging_indent == -1) {
+            $count-- if (m/^(\s*)([a-z_0-9]+):/ && $2 ne "default"); # label
+        }
     }
     if($count %INDENT_LEVEL != 0 && $count != $hanging_indent) { # well, does not detect indentation off by multiples of INDENT_LEVEL
         print "$ARGV:$line:indent: $_";
@@ -79,20 +79,20 @@ while(<>) {
             $in_multiline_comment = 1;
         }
     }
-    if(!$in_multiline_comment && m/[^\s]\s*\{\s*$/ && !m/\}/) { # trailing ... {
-        $line_opening_brace = $line;
-        $contents_2_lines_before = $_;
-    }
-    if(!$in_multiline_comment && m/\}[\s;]*$/) { # trailing ... }
-        my $line_2_before = $line-2;
-        if($line_opening_brace &&
-           $line_opening_brace == $line_2_before) {
-            print "$ARGV:$line_2_before:{1 line}: $contents_2_lines_before";
+    if(!$in_multiline_comment) {
+        if (m/[^\s]\s*\{\s*$/ && !m/\}/) { # trailing ... {
+            $line_opening_brace = $line;
+            $contents_2_lines_before = $_;
         }
-        $line_opening_brace = 0;
-    }
-  MATCH_PAREN:
-    if (!$in_multiline_comment) {
+        if(m/\}[\s;]*$/) { # trailing ... }
+            my $line_2_before = $line-2;
+            if($line_opening_brace &&
+               $line_opening_brace == $line_2_before) {
+                print "$ARGV:$line_2_before:{1 line}: $contents_2_lines_before";
+            }
+            $line_opening_brace = 0;
+        }
+      MATCH_PAREN:
         if (m/^(.*)\(([^\(]*)$/) { # last '('
             my $head = $1;
             my $tail = $2;
@@ -113,12 +113,12 @@ while(<>) {
         } elsif ($count != $hanging_indent) {
             $hanging_indent = -1; # reset hanging indent
         }
-    }
-    if (!$in_multiline_comment && $hanging_indent == -1 &&
-        m/^(\s*)(((\w+|\*)\s*)+=\s*)[^;]*\s*$/) { # multi-line assignment: "[type] var = " without ;
-        my $head = $1;
-        my $var_eq = $2;
-        $hanging_indent = length($head) + length($var_eq);
+        if ($hanging_indent == -1 &&
+            m/^(\s*)(((\w+|\*)\s*)+=\s*)[^;]*\s*$/) { # multi-line assignment: "[type] var = " without ;
+            my $head = $1;
+            my $var_eq = $2;
+            $hanging_indent = length($head) + length($var_eq);
+        }
     }
 
     $line = 0 if eof;
