0f9f8e96db49dc70ee5343d14719ed296fba175e
Dr. David von Oheimb <David.von.Oheimb@siemens.com>
2019-12-03 11:15:36 +0100

    correct naming in diagnostics of cmp_vfyc.c: server cert -> sender cert

--- include/openssl/cmperr.h
+++ include/openssl/cmperr.h
@@ -93,7 +93,7 @@ int ERR_load_CMP_strings(void);
 #  define CMP_R_MULTIPLE_SAN_SOURCES                       102
 #  define CMP_R_MISSING_SENDER_IDENTIFICATION              111
 #  define CMP_R_NO_STDIO                                   194
-#  define CMP_R_NO_SUITABLE_SERVER_CERT                    161
+#  define CMP_R_NO_SUITABLE_SENDER_CERT                    161
 #  define CMP_R_SRVCERT_DOES_NOT_VALIDATE_MSG              162
 #  define CMP_R_NULL_ARGUMENT                              103
 #  define CMP_R_PKIBODY_ERROR                              163
