ce33e7b30f727786a8799a2bccbb5117a3f1d259
Dr. David von Oheimb <David.von.Oheimb@siemens.com>
2019-12-03 07:29:34 +0100

    correct error msg for srvCert not validating msg in cmp_vfy.c

--- include/openssl/cmperr.h
+++ include/openssl/cmperr.h
@@ -94,7 +94,7 @@ int ERR_load_CMP_strings(void);
 #  define CMP_R_MISSING_SENDER_IDENTIFICATION              111
 #  define CMP_R_NO_STDIO                                   194
 #  define CMP_R_NO_SUITABLE_SERVER_CERT                    161
-#  define CMP_R_NO_VALID_SERVER_CERT_FOUND                 162
+#  define CMP_R_SRVCERT_DOES_NOT_VALIDATE_MSG              162
 #  define CMP_R_NULL_ARGUMENT                              103
 #  define CMP_R_PKIBODY_ERROR                              163
 #  define CMP_R_PKICONF_NOT_RECEIVED                       164
