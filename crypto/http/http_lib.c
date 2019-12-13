514c048de833ee71a67c474aab600e86fb39d620
Dr. David von Oheimb <David.von.Oheimb@siemens.com>
2019-12-13 07:43:46 +0100

    allow NULL variable output pointers for OSSL_HTTP_parse_url()

--- crypto/http/http_lib.c
+++ crypto/http/http_lib.c
@@ -21,17 +21,22 @@ int OSSL_HTTP_parse_url(const char *url, char **phost, char **pport,
                         char **ppath, int *pssl)
 {
     char *p, *buf;
-    char *host, *port;
+    char *host;
+    char *port = "80";
 
-    if (url == NULL
-        || phost == NULL || pport == NULL || ppath == NULL || pssl == NULL) {
+    if (url == NULL) {
         HTTPerr(0, ERR_R_PASSED_NULL_PARAMETER);
         return 0;
     }
 
-    *phost = NULL;
-    *pport = NULL;
-    *ppath = NULL;
+    if (phost != NULL)
+        *phost = NULL;
+    if (pport != NULL)
+        *pport = NULL;
+    if (ppath != NULL)
+        *ppath = NULL;
+    if (pssl != NULL)
+        *pssl = 0;
 
     /* dup the buffer since we are going to mess with it */
     if ((buf = OPENSSL_strdup(url)) == NULL)
@@ -39,36 +44,32 @@ int OSSL_HTTP_parse_url(const char *url, char **phost, char **pport,
 
     /* Check for initial colon */
     p = strchr(buf, ':');
-    if (p == NULL)
-        goto parse_err;
-    *(p++) = '\0';
+    if (p == NULL || p - buf > 5 /* strlen("https") */) {
+        p = buf;
+    } else {
+        *(p++) = '\0';
 
-    if (strcmp(buf, "http") == 0) {
-        *pssl = 0;
-        port = "80";
-    } else if (strcmp(buf, "https") == 0) {
-        *pssl = 1;
-        port = "443";
-    } else
-        goto parse_err;
+        if (strcmp(buf, "https") == 0) {
+            if (pssl != NULL)
+                *pssl = 1;
+            port = "443";
+        }
+        else if (strcmp(buf, "http") != 0)
+            goto parse_err;
 
-    /* Check for double slash */
-    if ((p[0] != '/') || (p[1] != '/'))
-        goto parse_err;
-    p += 2;
+        /* Check for double slash */
+        if ((p[0] != '/') || (p[1] != '/'))
+            goto parse_err;
+        p += 2;
+    }
     host = p;
 
     /* Check for trailing part of path */
     p = strchr(p, '/');
-    if (p == NULL)
-        *ppath = OPENSSL_strdup("/");
-    else {
-        *ppath = OPENSSL_strdup(p);
-        /* Set start of path to 0 so hostname is valid */
-        *p = '\0';
-    }
-    if (*ppath == NULL)
+    if (ppath != NULL && (*ppath = OPENSSL_strdup(p == NULL ? "/" : p)) == NULL)
         goto err;
+    if (p != NULL)
+        *p = '\0'; /* Set start of path to 0 so hostname[:port] is valid */
 
     p = host;
     if (host[0] == '[') {
@@ -80,15 +81,15 @@ int OSSL_HTTP_parse_url(const char *url, char **phost, char **pport,
         *p = '\0';
         p++;
     }
-    if ((*phost = OPENSSL_strdup(host)) == NULL)
-        goto err;
 
     /* Look for optional ':' for port number */
     if ((p = strchr(p, ':'))) {
-        *p = 0;
+        *p = '\0';
         port = p + 1;
     }
-    if ((*pport = OPENSSL_strdup(port)) == NULL)
+    if (phost != NULL && (*phost = OPENSSL_strdup(host)) == NULL)
+        goto err;
+    if (pport != NULL && (*pport = OPENSSL_strdup(port)) == NULL)
         goto err;
 
     OPENSSL_free(buf);
@@ -98,12 +99,18 @@ int OSSL_HTTP_parse_url(const char *url, char **phost, char **pport,
     HTTPerr(0, HTTP_R_ERROR_PARSING_URL);
 
  err:
-    OPENSSL_free(*ppath);
-    *ppath = NULL;
-    OPENSSL_free(*pport);
-    *pport = NULL;
-    OPENSSL_free(*phost);
-    *phost = NULL;
+    if (ppath != NULL) {
+        OPENSSL_free(*ppath);
+        *ppath = NULL;
+    }
+    if (pport != NULL) {
+        OPENSSL_free(*pport);
+        *pport = NULL;
+    }
+    if (phost != NULL) {
+        OPENSSL_free(*phost);
+        *phost = NULL;
+    }
     OPENSSL_free(buf);
     return 0;
 }
